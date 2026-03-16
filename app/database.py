f7115700026008bbf84d0c725f8c1221ef113987
JoaoRoccella <joao.a@docente.senai.br>
2026-03-16 11:43:43 -0300

    fix: carregamento das variáveis de ambiente

--- app/database.py
+++ app/database.py
@@ -1,24 +1,29 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 from os import getenv
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+# Definindo o caminho de sistema do .env
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # Inicialização (criação do SCHEMA no banco de dados)
-# SERVER_URL = f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PSWD')}@{getenv('DB_HOST')}"
-SERVER_URL = "mysql+pymysql://root:admin@localhost"
+SERVER_URL = (
+    f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PSWD')}@{getenv('DB_HOST')}"
+)
+# SERVER_URL = "mysql+pymysql://root:admin@localhost"
 
 engine_server = create_engine(SERVER_URL)
 
 with engine_server.connect() as conn:
-    conn.execute(text("CREATE DATABASE IF NOT EXISTS series_api"))
+    conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {getenv('DB_NAME')}"))
     conn.commit()
 
 
 # Conexão com o banco já criado
-# DATABASE_URL = f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PSWD')}@{getenv('DB_HOST')}/{getenv('DB_NAME')}"
-DATABASE_URL = "mysql+pymysql://root:admin@localhost/series_api"
+DATABASE_URL = f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PSWD')}@{getenv('DB_HOST')}/{getenv('DB_NAME')}"
+# DATABASE_URL = "mysql+pymysql://root:admin@localhost/series_api"
 
 # Criar um "motor" que fará o gerenciamento da conexão
 engine = create_engine(DATABASE_URL)
@@ -28,6 +33,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Cria um objeto da base de dados manipulável pelo Python
 Base = declarative_base()
+
 
 # Injeção de dependência: injeta a sessão do banco de dados
 # em cada rota que for criada.
